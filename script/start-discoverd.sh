987144d13c8c13b34a8ba74c23a8833a6c32973c
Randy Girard <randy@tradepending.com>
2025-11-26 20:36:07 -0500

    chore: update to track pids

--- script/start-discoverd.sh
+++ script/start-discoverd.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+PIDDIR="/var/run/flynn-local"
+mkdir -p "$PIDDIR"
+
 discoverd \
   -data-dir=/tmp/discoverd-data \
   -host=192.0.2.200 \
@@ -8,3 +11,4 @@ discoverd \
   -notify="http://192.0.2.200:1113/host/discoverd" \
   -wait-net-dns=true \
   > /tmp/discoverd.log 2>&1 &
+echo $! > "$PIDDIR/discoverd.pid"
--- script/start-flanneld.sh
+++ script/start-flanneld.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+PIDDIR="/var/run/flynn-local"
+mkdir -p "$PIDDIR"
+
 flanneld \
   -discoverd-url="http://${DISCOVERD}" \
   -iface="${EXTERNAL_IP}" \
@@ -8,9 +11,11 @@ flanneld \
   -notify-url="http://${EXTERNAL_IP}:1113/host/network" \
   -logtostderr \
   > /tmp/flanneld.log 2>&1 &
+echo $! > "$PIDDIR/flanneld.pid"
 
 EXTERNAL_IP=192.0.2.200 \
   PORT=5002 \
   NETWORK=100.100.0.0/16 \
   flannel-wrapper \
   > /tmp/flanneld-wrapper.log 2>&1 &
+echo $! > "$PIDDIR/flanneld-wrapper.pid"
