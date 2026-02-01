8c10cef10384bdb3656842b7d3f8227da608ffd3
Randy Girard <randy@tradepending.com>
2026-02-01 18:59:38 -0500

    chore: update bin directory

--- appliance/mariadb/process.go
+++ appliance/mariadb/process.go
@@ -33,7 +33,7 @@ import (
 
 const (
 	DefaultPort        = "3306"
-	DefaultBinDir      = "/bin"
+	DefaultBinDir      = "/usr/bin"
 	DefaultSbinDir     = "/usr/sbin"
 	DefaultDataDir     = "/data"
 	DefaultPassword    = ""
